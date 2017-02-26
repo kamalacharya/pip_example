68ef84d26b154389419364c48462e293b3ff23dd
Kamal Acharya <kamal@thumbtack.com>
2017-02-25 20:12:37 -0800

    Use check_call instead of check_output

--- pip_example/scripts/pip_example.py
+++ pip_example/scripts/pip_example.py
@@ -11,6 +11,5 @@ def main():
     print 'current dir = %s' % SCRIPT_DIR
 
     print "Executing bash script ../bash_scripts/get_env.sh..."
-    get_env_result = check_output([SCRIPT_DIR + '/../bash_scripts/get_env.sh'])
-    print get_env_result
+    check_call([SCRIPT_DIR + '/../bash_scripts/get_env.sh'])
 
